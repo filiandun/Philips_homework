a253446709895763e93c5ecabcd31cff148ea087
filian <filiandun@gmail.com>
2022-11-06 21:27:08 +0300

    void

--- HW/HW/HW.cpp
+++ HW/HW/HW.cpp
@@ -1,8 +1,18 @@
 ﻿#include <iostream>
 
+void num();
+
 int main()
 {
     std::cout << "Hello World!\n";
 
+    num();
+
     return 0;
+}
+
+void num() {
+    for (int i = 0; i < 5; ++i) {
+	cout << i << endl;
+    }
 }
